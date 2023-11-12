4515e986a756a80bec1d5f06de587140dcdb295d
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-11-12 13:23:26 +0200

    feat: added eslint config (#4)
    * feat: added eslint config
    
    * refactor: updated package.json file
    
    * refactor: updated config.yml

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: 'mongodb+srv://peterkulko:IOeJH2uyQBU0zDDj@cluster0.dakktrl.mongodb.net/coffee-shop?retryWrites=true&w=majority',
-    USER_ID: '654686239c1d584cd2e32f38',
-}
+  MONGODB_URI: 'mongodb+srv://peterkulko:IOeJH2uyQBU0zDDj@cluster0.dakktrl.mongodb.net/coffee-shop?retryWrites=true&w=majority',
+  USER_ID: '654686239c1d584cd2e32f38',
+};
