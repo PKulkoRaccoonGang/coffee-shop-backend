b9535c3a3876a688de9ebcc22252880e476f1184
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-11-13 00:18:45 +0200

    fix: added sendgrid api env variable (#6)

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,5 +1,6 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   USER_ID: process.env.USER_ID,
+  SEND_GRID_API_KEY: process.env.SEND_GRID_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
 };
