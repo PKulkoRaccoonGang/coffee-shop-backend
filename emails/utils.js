64dc50c2a5c98ef61f2f16cc9446f46a30bb1e0c
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-11-13 10:35:11 +0200

    refactor: some refactoring

--- emails/utils.js
+++ emails/utils.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Reads the content of an HTML template from a file.
+ *
+ * @param {string} templateFileName - The name of the HTML template file.
+ * @returns {string} - The content of the HTML template as a string.
+ * @throws {Error} - If the file is not found or an error occurs during reading.
+ */
 const readHtmlTemplate = (templateFileName) => {
   const filePath = path.join(__dirname, `templates/${templateFileName}`);
   return fs.readFileSync(filePath, 'utf-8');
--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://peterkulko:IOeJH2uyQBU0zDDj@cluster0.dakktrl.mongodb.net/coffee-shop?retryWrites=true&w=majority',
   USER_ID: '654686239c1d584cd2e32f38',
-  SEND_GRID_API_KEY: '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',
+  SEND_GRID_API_KEY: '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',
   EMAIL_FROM: 'petrokulko@gmail.com',
 };
